d2657d41d556d5bab20e0473ac6e5cd434c31e15
Graham Markall <graham.markall@embecosm.com>
2018-08-23 11:00:18 +0100

    Build done

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 
 from compile_args import compile_args
@@ -11,20 +12,20 @@ def run(cmd, **kwargs):
 
 def clean():
     print("Cleaning")
-    cmd = [ 'rm -f f*.ll f*.s test?-?-? test?-??-? test??-?-? '
-            'test??-??-? gdb_output/gdb_output_test*' ]
+    cmd = [ 'rm -rf output' ]
     run(cmd, shell=True)
+    os.mkdir('output')
 
 def prepare(balance):
     print("Preparing. Balance: %s" % balance)
     for i in range(1, 13):
         clang = [ 'clang', '-S', '-emit-llvm',
-            './functions/f%d.c' % i, '-o', 'f%d.ll' % i ]
+            './functions/f%d.c' % i, '-o', 'output/f%d.ll' % i ]
         run(clang)
 
         balance_arg = 1 if balance else 0
-        llvm = [ 'llc', 'f%d.ll' % i, '-riscv-cfg-balance=%d' % balance_arg,
-            '-o', 'f%d.s' % i ]
+        llvm = [ 'llc', 'output/f%d.ll' % i, '-riscv-cfg-balance=%d' % balance_arg,
+            '-o', 'output/f%d.s' % i ]
         run(llvm)
 
 def make_all_tests():
@@ -32,8 +33,8 @@ def make_all_tests():
     gcc = 'riscv32-unknown-elf-gcc'
     for ((test, instance, variant), args) in compile_args:
         src = './test-cases/main%d-%d.c' % (test, instance)
-        asm = 'f%d.s' % test
-        exe = 'test%d-%d-%d' % (test, instance, variant)
+        asm = 'output/f%d.s' % test
+        exe = 'output/test%d-%d-%d' % (test, instance, variant)
         args = [ '-DARG%d=%d' % (i+1, arg) for i, arg in enumerate(args) ]
         cmd = [ gcc, src, asm, '-g', '-o', exe ]
         cmd += args
